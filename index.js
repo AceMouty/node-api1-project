0e56d556233acdb2610d137361a672842df5c82f
AceMouty <kyleguerrero85@gmail.com>
2019-09-30 14:19:29 -0500

    Built out a basic server
    basic server created and listening on port 8000

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // implement your API here
+
+// import express
+const express = require('express');
+const server = express()
+
+const port = 8000;
+server.listen(port, () => console.log(` API IT LISTENING ON PORT ${port}`))
